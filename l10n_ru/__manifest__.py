433ad575b21fa6d7002e301b3c131311149a18e7
Katulos <123748719+Katulos@users.noreply.github.com>
2025-01-17 10:48:45 +0000

    Fix manifest

--- l10n_ru/__manifest__.py
+++ l10n_ru/__manifest__.py
@@ -1,14 +1,16 @@
 {
     "name": "Russia - Accounting",
-    "website": "https://github.com/OCA/l10n-russia",
-    "countries": ["ru"],
+    "icon": "/account/static/description/l10n.png",
     "version": "17.0.1.0.0",
+    "countries": ["ru"],
     "category": "Accounting/Localizations/Account Charts",
+    "website": "https://github.com/OCA/l10n-russia",
+    "depends": ["account"],
     "author": "CodUP, Katulos, Odoo Community Association (OCA)",
     "maintainers": ["Katulos"],
-    "license": "AGPL-3",
-    "depends": ["account"],
     "data": [
         "data/account_account_tag_data.xml",
     ],
+    "installable": True,
+    "license": "AGPL-3",
 }
